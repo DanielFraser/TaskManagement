46ac8fbf87c6ca4f2ca01c025cd2df302a140dec
Daniel R Fraser <dukeydk@gmail.com>
2019-02-24 17:17:04 -0500

    Added initial files

--- webapitest/App_Start/WebApiConfig.cs
+++ webapitest/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace webapitest
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- webapitest/Controllers/ValuesController.cs
+++ webapitest/Controllers/ValuesController.cs
@@ -9,31 +9,51 @@ namespace webapitest.Controllers
 {
     public class ValuesController : ApiController
     {
+        private static Dictionary<int, string> dict = new Dictionary<int, string>();
+        private static List<string> a = new List<string>();
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return a;
         }
+
+        [Route("values/get2/{str}")]
+        public string Get2(string str)
+        {
+            return str;
+        }
+
+        //public IHttpActionResult GetAll()
+        //{
+        //    if (dict.Values.Count() > 0)
+        //        return Ok(dict.Values);
+        //    return NotFound();
+        //}
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            if (dict.ContainsKey(id))
+                return dict[id];
+            return id.ToString();
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            a.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            dict.Add(id, "value"+id);
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+           
         }
     }
 }
